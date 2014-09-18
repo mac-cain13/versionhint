e2402b8126e6a893cb0930618742b8ae4a85b18a
Mathijs Kadijk <mkadijk@gmail.com>
2014-09-18 10:54:23 +0200

    First working Chrome version

---
+++ chrome/platformData.js
@@ -0,0 +1,153 @@
+var versionHint = {
+  'platformData': [
+    {
+      'name': 'iOS',
+      'tags': [
+               'ios', 'ios8', 'ios7', 'ios6', 'ios5.1', 'ios5', 'ios4', 'ios3',
+               'objective-c', 'swift', 'cocoa', 'cocoa-touch', 'uikit', 'coredata', 'xcode', 'core-animation', 'core-graphics',
+               'iphone', 'ipad', 'ipad-mini', 'ipod', 'ipod-touch'
+              ],
+      'versions': [
+        {
+          'name': 'iOS 8',
+          'fromDate': '2014/09/17'
+        },
+        {
+          'name': 'iOS 8 Beta',
+          'fromDate': '2014/06/02',
+          'toDate': '2014/09/16'
+        },
+
+        {
+          'name': 'iOS 7',
+          'fromDate': '2013/09/18',
+          'toDate': '2014/09/16'
+        },
+        {
+          'name': 'iOS 7 Beta',
+          'fromDate': '2013/06/10',
+          'toDate': '2013/09/17'
+        },
+
+        {
+          'name': 'iOS 6',
+          'fromDate': '2012/09/19',
+          'toDate': '2013/09/17'
+        },
+        {
+          'name': 'iOS 6 Beta',
+          'fromDate': '2012/06/11',
+          'toDate': '2012/09/18'
+        },
+
+        {
+          'name': 'iOS 5',
+          'fromDate': '2011/10/12',
+          'toDate': '2012/09/18'
+        },
+        {
+          'name': 'iOS 5 Beta',
+          'fromDate': '2011/06/06',
+          'toDate': '2011/10/11'
+        },
+
+        {
+          'name': 'iOS 4',
+          'fromDate': '2010/06/21',
+          'toDate': '2011/10/11'
+        },
+
+        {
+          'name': 'iPhone OS 3',
+          'fromDate': '2009/06/17',
+          'toDate': '2010/06/20'
+        },
+
+        {
+          'name': 'iPhone OS 2',
+          'fromDate': '2008/06/11',
+          'toDate': '2009/06/16'
+        }
+      ]
+    },
+
+    {
+      'name': 'Android',
+      'tags': [
+               'android'
+              ],
+      'versions': [
+        {
+          'name': 'Android 4.4 KitKat (API level 19)',
+          'fromDate': '2013/10/31'
+        },
+
+        {
+          'name': 'Android 4.3 Jelly Bean (API level 18)',
+          'fromDate': '2013/07/24',
+          'toDate': '2013/10/30'
+        },
+        {
+          'name': 'Android 4.2 Jelly Bean (API level 17)',
+          'fromDate': '2012/10/29',
+          'toDate': '2013/07/23'
+        },
+        {
+          'name': 'Android 4.1 Jelly Bean (API level 16)',
+          'fromDate': '2012/06/27',
+          'toDate': '2012/10/28'
+        },
+
+        {
+          'name': 'Android 4.0 ICS (API level 15)',
+          'fromDate': '2011/12/16',
+          'toDate': '2012/06/26'
+        },
+        {
+          'name': 'Android 4.0 ICS (API level 14)',
+          'fromDate': '2011/10/18',
+          'toDate': '2011/12/15'
+        },
+
+        {
+          'name': 'Android 3.2 Honeycomb (API level 13)',
+          'fromDate': '2011/07/15',
+          'toDate': '2011/10/17'
+        },
+        {
+          'name': 'Android 3.1 Honeycomb (API level 12)',
+          'fromDate': '2011/05/10',
+          'toDate': '2011/07/14'
+        },
+        {
+          'name': 'Android 3.0 Honeycomb (API level 11)',
+          'fromDate': '2011/02/22',
+          'toDate': '2011/05/09'
+        },
+
+        {
+          'name': 'Android 2.3 Gingerbread (API level 10)',
+          'fromDate': '2011/02/09',
+          'toDate': '2011/10/17'
+        },
+        {
+          'name': 'Android 2.3 Gingerbread (API level 9)',
+          'fromDate': '2010/12/06',
+          'toDate': '2011/02/08'
+        },
+
+        {
+          'name': 'Android 2.2 Froyo (API level 8)',
+          'fromDate': '2010/05/20',
+          'toDate': '2010/12/05'
+        },
+
+        {
+          'name': 'Android 2.1 Froyo (API level 8)',
+          'fromDate': '2010/05/20',
+          'toDate': '2010/12/05'
+        }
+      ]
+    }
+  ]
+};
